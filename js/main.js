b9f76f1596281ce458e647a37c18878ceffa8ee1
eu81273 <eu81273@gmail.com>
2016-12-02 18:14:38 +0900

    공통 로직 분리

--- js/main.js
+++ js/main.js
@@ -16,30 +16,8 @@ window.onload = function() {
         reader.readAsDataURL(fileList[0]);
 
         //로드 한 후
-        reader.onload = function() {
-            //썸네일 이미지 생성
-            var tempImage = new Image(); //drawImage 메서드에 넣기 위해 이미지 객체화
-            tempImage.src = reader.result; //data-uri를 이미지 객체에 주입
-            tempImage.onload = function() {
-                //이미지를 캔버스에 그리기
-                context.drawImage(this, 0, 0, 500, 500);
-
-                var candleImage = new Image();
-                candleImage.src = '/img/candle.png';
-                candleImage.onload = function() {
-                    //이미지를 캔버스에 그리기
-                    context.drawImage(this, 0, 0, 500, 500);
-
-                    //캔버스에 그린 이미지를 다시 data-uri 형태로 변환
-                    var dataURI = canvas.toDataURL("image/jpeg");
-
-                    //썸네일 이미지 보여주기
-                    document.querySelector('#preview').src = dataURI;
-
-                    //썸네일 이미지를 다운로드할 수 있도록 링크 설정
-                    document.querySelector('#download').href = dataURI;
-                };
-            };
+        reader.onload = function () {
+            createProfile(reader.result);
         };
     };
 
@@ -52,35 +30,36 @@ function file() {
 function myFacebookLogin() {
     FB.login(function() {
         FB.api('/me/picture?width=500&height=500', function(response) {
-
-            //썸네일 이미지 생성
-            var tempImage = new Image(); //drawImage 메서드에 넣기 위해 이미지 객체화
-            tempImage.setAttribute('crossOrigin', 'anonymous');
-            tempImage.src = response.data.url; //data-uri를 이미지 객체에 주입
-            tempImage.onload = function() {
-                //이미지를 캔버스에 그리기
-                context.drawImage(this, 0, 0, 500, 500);
-
-                var candleImage = new Image();
-                candleImage.src = '/img/candle.png';
-                candleImage.onload = function() {
-                    //이미지를 캔버스에 그리기
-                    context.drawImage(this, 0, 0, 500, 500);
-
-                    //캔버스에 그린 이미지를 다시 data-uri 형태로 변환
-                    var dataURI = canvas.toDataURL("image/jpeg");
-
-                    //썸네일 이미지 보여주기
-                    document.querySelector('#preview').src = dataURI;
-
-                    //썸네일 이미지를 다운로드할 수 있도록 링크 설정
-                    document.querySelector('#download').href = dataURI;
-                };
-            };
+            createProfile(response.data.url);
         });
-
-
     }, {
         scope: 'user_about_me'
     });
+}
+
+function createProfile (profile) {
+    //썸네일 이미지 생성
+    var tempImage = new Image(); //drawImage 메서드에 넣기 위해 이미지 객체화
+    tempImage.setAttribute('crossOrigin', 'anonymous'); //크로스 오리진 설정
+    tempImage.src = profile; //data-uri를 이미지 객체에 주입
+    tempImage.onload = function() {
+        //이미지를 캔버스에 그리기
+        context.drawImage(this, 0, 0, 500, 500);
+
+        var candleImage = new Image();
+        candleImage.src = '/img/candle.png';
+        candleImage.onload = function() {
+            //이미지를 캔버스에 그리기
+            context.drawImage(this, 0, 0, 500, 500);
+
+            //캔버스에 그린 이미지를 다시 data-uri 형태로 변환
+            var dataURI = canvas.toDataURL("image/jpeg");
+
+            //썸네일 이미지 보여주기
+            document.querySelector('#preview').src = dataURI;
+
+            //썸네일 이미지를 다운로드할 수 있도록 링크 설정
+            document.querySelector('#download').href = dataURI;
+        };
+    };
 }
